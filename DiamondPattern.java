811785313380fa78ac3412bc407a614829d104a6
pallavi-gatade <pallavi.gatade24@gmail.com>
2025-02-07 18:23:15 +0530

    Add files via upload

---
+++ DiamondPattern.java
@@ -0,0 +1,52 @@
+package patternPrograms;
+
+public class DiamondPattern {
+
+	public static void main(String[] args) {
+
+		int n = 5;
+
+		for (int i = 1; i <= n; i++) {
+
+			for (int j = i; j <= n; j++) {
+				System.out.print("  ");
+			}
+
+			for (int j = 1; j < i; j++) {
+				System.out.print("* ");
+			}
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("* ");
+			}
+
+			System.out.println();
+		}
+		for (int i = 2; i <= n; i++) {
+
+			for (int j = 1; j <= i; j++) {
+				System.out.print("  ");
+			}
+
+			for (int j = i; j <= n; j++) {
+				System.out.print("* ");
+			}
+
+			for (int j = i; j < n; j++) {
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+}
+
+//output
+//        * 
+//      * * * 
+//    * * * * * 
+//  * * * * * * * 
+//* * * * * * * * * 
+//  * * * * * * * 
+//    * * * * * 
+//	    * * * 
+//		  * 
